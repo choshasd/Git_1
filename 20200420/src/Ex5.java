d332d7f01d0c9eb0e2c401fe733170477712dc22
305 <305@305-PC>
2020-04-20 12:02:47 +0900

    second

--- 20200420/src/Ex5.java
+++ 20200420/src/Ex5.java
@@ -1,6 +1,30 @@
+import java.util.Scanner;
 
 public class Ex5 {
 	public static void main(String[] args) {
-		System.out.println("스위치 공부하자");
+		System.out.println("번호를 입력하세요");
+		Scanner scan = new Scanner(System.in);
+
+		int input = scan.nextInt();
+		scan.close();
+		
+		// ctrl + shift + f : 자동정렬
+		switch (input) {
+		case 1:
+			System.out.println("가위");
+			break;
+			
+		case 2:
+			System.out.println("바위");
+			break;
+			
+		case 3:
+			System.out.println("보");
+			break;
+
+		default:
+			System.out.println("1,2,3 중에 입력하여주세요");
+			break;
+		}
 	}
 }
