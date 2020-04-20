297d459409f8223f19a6a9e240de87030cf31ce3
305 <305@305-PC>
2020-04-20 12:25:42 +0900

    second_

--- 20200420/src/Ex5.java
+++ 20200420/src/Ex5.java
@@ -2,14 +2,17 @@ import java.util.Scanner;
 
 public class Ex5 {
 	public static void main(String[] args) {
+		
 		System.out.println("번호를 입력하세요");
 		Scanner scan = new Scanner(System.in);
-
-		int input = scan.nextInt();
 		scan.close();
+		
+		int input = scan.nextInt();
+		
 		
 		// ctrl + shift + f : 자동정렬
 		switch (input) {
+		
 		case 1:
 			System.out.println("가위");
 			break;
